8aea5393a7e8d8481ec02baf36575507a7fbf70e
Pradeep Dubey <pradeepdubey82@gmail.com>
2025-06-26 15:00:08 +0530

    Update test.js

--- blocks/test/test.js
+++ blocks/test/test.js
@@ -2,7 +2,7 @@ export default function decorate(block) {
 const [quoteWrapper] = block.children;
 
   const blockquote = document.createElement('blockquote');
-  blockquote.listClass.add('title');
+  blockquote.classList.add('title');
   blockquote.textContent = quoteWrapper.textContent.trim();
   quoteWrapper.replaceChildren(blockquote);
 }
