9b5f789ea869bfc68b46430fc8c8d81d84e471aa
Pradeep Dubey <pradeepdubey82@gmail.com>
2025-06-26 14:44:42 +0530

    Update test.js

--- blocks/test/test.js
+++ blocks/test/test.js
@@ -1,14 +1,4 @@
 export default function decorate(block) { 
-  const cells = Array.from(block.children);
-  const t1 = cells[0].classList.add('title');
-  const t2 = cells[1].classList.add('description');
 
-  const initialValue = '5';
-  const counter = document.createElement('counter-element');
-  if (initialValue) {
-    counter.setAttribute('initial-value', initialValue);
-  }
-  block.textContent = '';
-  block.appendChild(counter);
 
 }
