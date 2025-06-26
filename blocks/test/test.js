e7d3a0753fd48920e9d4787f2ad632ecc7db9d8c
Pradeep Dubey <pradeepdubey82@gmail.com>
2025-06-26 14:16:46 +0530

    Update test.js

--- blocks/test/test.js
+++ blocks/test/test.js
@@ -1,10 +1,7 @@
 export default function decorate(block) { 
   block.querySelectorAll('p').forEach((p) => {
     const innerHTML = p.innerHTML?.trim();
-
-    // If the paragraph starts with Terms and conditions: then style it as such
-    if (innerHTML?.startsWith("Title:")) {
-      /* If a paragraph starts with '*', add a special CSS class. */
+    if (innerHTML?.startsWith("Title")) {
       p.classList.add('title');
     }
   });
