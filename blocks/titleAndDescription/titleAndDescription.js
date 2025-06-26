0df62fce65258cc2abe5221fcb27e78daccba28a
pradeep dubey <pradeep.dubey@sap.com>
2025-06-26 10:57:47 +0530

    updated css and js

---
+++ blocks/titleAndDescription/titleAndDescription.js
@@ -0,0 +1,8 @@
+export default function decorate(block) {
+    alert("js invoked!!");
+    block.chi
+    const title = document.createElement("span");
+    title.classList.add("title");
+    const description = document.createElement("span");
+    description.classList.add("description");
+}
