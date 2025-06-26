d4c5d67e3f9259cb478f984892e3f17472e59ca8
pradeep dubey <pradeep.dubey@sap.com>
2025-06-26 12:44:52 +0530

    test

--- blocks/test/test.js
+++ blocks/test/test.js
@@ -1,6 +1,6 @@
 export default function decorate(block) { 
-  const title = block.title.classList.add("title");
-  const desc = block.description.classList.add("description");
+  const title = block.title.classList.add('title');
+  const desc = block.description.classList.add('description');
   block.appendChild(desc);
   block.children.appendChild(title);
 }
