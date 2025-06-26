47e127e8b3c1617a96cb739b55c5814dd7f7fd70
Pradeep Dubey <pradeepdubey82@gmail.com>
2025-06-26 15:47:51 +0530

    Update test.js

--- blocks/test/test.js
+++ blocks/test/test.js
@@ -2,6 +2,9 @@ export default function decorate(block) {
 
 const elements = [...block.children];
   console.log("Elements ::: "+elements);
+  Array.from([...block.children]).forEach(function (element) {
+    console.log(element.getAttribute('data-aue-prop'));
+});
 const [titleElement] = block.children;
   const dummy = document.createElement('dummy');
   dummy.classList.add('title');
