0cb0cf1dda0f49fa1a82d6305da392d580caf263
pradeep dubey <pradeep.dubey@sap.com>
2025-06-26 11:26:19 +0530

    test

--- blocks/titleAndDescription/titleAndDescription.js
+++ blocks/titleAndDescription/titleAndDescription.js
@@ -8,4 +8,16 @@ export default function decorate(block) {
     const descriptioText = cells[1]?.textContent.trim();
     descriptioText.classList.add("titleAndDescription description");
     block.appendChild(descriptioText);
+
+    block.querySelectorAll('p').forEach((p) => {
+    const innerHTML = p.innerHTML?.trim();
+
+    // If the paragraph starts with Terms and conditions: then style it as such
+    if (innerHTML?.startsWith("Title")) {
+      /* If a paragraph starts with '*', add a special CSS class. */
+      p.classList.add('title');
+    } else {
+        p.classList.add('description');
+    }
+  });
 }
