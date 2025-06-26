9e2c96d6c2f9b905b3e0f486f04359758980e3ab
Pradeep Dubey <pradeepdubey82@gmail.com>
2025-06-26 15:18:50 +0530

    Update test.js

--- blocks/test/test.js
+++ blocks/test/test.js
@@ -1,6 +1,6 @@
 export default function decorate(block) {
 const [titleElement] = block.children;
-
+alert('testing');
   const dummy = document.createElement('dummy');
   dummy.classList.add('title');
   dummy.textContent = titleElement.textContent.trim();
