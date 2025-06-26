a181b5ab068f22b6e61474c2e56ea66539f93c54
Pradeep Dubey <pradeepdubey82@gmail.com>
2025-06-26 15:54:06 +0530

    Update test.js

--- blocks/test/test.js
+++ blocks/test/test.js
@@ -8,10 +8,11 @@ let collection = [...block.children];
         for (let i of collection) {
             console.log(collection[i].getAttribute('data-aue-prop'));
         }
-
+/*
 const [titleElement] = block.children;
   const dummy = document.createElement('dummy');
   dummy.classList.add('title');
   dummy.textContent = titleElement.textContent.trim();
   titleElement.replaceChildren(dummy);
+  */
 }
