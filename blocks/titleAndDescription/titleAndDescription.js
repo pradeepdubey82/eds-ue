ddf2c1fc629904e9a0da8488ef1853a36a10befd
pradeep dubey <pradeep.dubey@sap.com>
2025-06-26 11:29:44 +0530

    test

--- blocks/titleAndDescription/titleAndDescription.js
+++ blocks/titleAndDescription/titleAndDescription.js
@@ -1,14 +1,4 @@
 export default function decorate(block) {
-    alert("js invoked!!");
-    const cells = Array.from(block.children);
-    const titleText = cells[0]?.textContent.trim();
-    titleText.classList.add("titleAndDescription title");
-    block.textContent = '';
-    block.appendChild(titleText);
-    const descriptioText = cells[1]?.textContent.trim();
-    descriptioText.classList.add("titleAndDescription description");
-    block.appendChild(descriptioText);
-
     block.querySelectorAll('p').forEach((p) => {
     const innerHTML = p.innerHTML?.trim();
 
